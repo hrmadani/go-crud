98394d14937402fab36465d60f83807e02178002
hrmadani <hrmadani2@gmail.com>
2022-04-15 12:10:08 +0430

    - Movie and Director data are defined - Server is start

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
 type Movie struct {
@@ -19,6 +22,10 @@ type Director struct {
 var movies []Movie
 
 func main() {
+	//Define Data
+	movies = append(movies, Movie{ID: "1", Isbn: "4323455", Title: "Movie One", Director: &Director{FirstName: "John", LastName: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "55432344", Title: "Movie Two", Director: &Director{FirstName: "Steve", LastName: "Mcmaneman"}})
+
 	r := mux.NewRouter()
 	//Routes
 	r.HandleFunc("/movies", getMovies).Methods("GET")
@@ -27,4 +34,8 @@ func main() {
 	r.HandleFunc("/movies/(id)", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/(id)", deleteMovie).Methods("DELETE")
 
+	fmt.Printf("Start the server on port 8000")
+	if err := http.ListenAndServe(":8000", r); err != nil {
+		log.Fatal(err)
+	}
 }
